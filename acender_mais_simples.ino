9158f5e874ee39644ca01b969dc81c553e991703
Beatriz Cesconetto <108144622+Beacesconetto@users.noreply.github.com>
2023-10-10 20:23:09 -0300

    Update acender_mais_simples.ino

--- acender_mais_simples.ino
+++ acender_mais_simples.ino
@@ -1,22 +1,21 @@
 void setup()
-2. {
-3.     // Porta 13 (LED_BUILTIN) em modo de saída (escrita)
-4.     pinMode(LED_BUILTIN, OUTPUT);
-5. }
-6.
-7. void loop()
-8. {
+{
+    // Porta 13 (LED_BUILTIN) em modo de saída (escrita)
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
   
-9.      // Aguarda 1000 milésimos de segundo      
+        // Aguarda 1000 milésimos de segundo      
         delay(1000); 
   
         // Desliga o LED
-10.     digitalWrite(LED_BUILTIN, LOW); 
+       digitalWrite(LED_BUILTIN, LOW); 
 
         // Aguarda 1000 milésimos de segundo
-11.     delay(1000);
+       delay(1000);
 
         // Liga o LED
-12.     digitalWrite(LED_BUILTIN, HIGH); 
-  
-13. }
+       digitalWrite(LED_BUILTIN, HIGH); 
+   }
